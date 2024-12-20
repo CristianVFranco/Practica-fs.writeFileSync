bdd9f57e5d84bceca06cbdfeeb9452e6d55ba42e
Franco <cvaldes272@gmail.com>
2024-12-19 20:04:50 -0600

    se agregan archivos de appendfilesync

--- read-file.js
+++ read-file.js
@@ -1,4 +1,4 @@
 const fs = require("fs");
-
+// readFileSync se utiliza para poder leer los archivos que mandemos a traer antes de encoding
 const data = fs.readFileSync("./Nuevo_archivo.txt", "utf8");
 console.log(data);
