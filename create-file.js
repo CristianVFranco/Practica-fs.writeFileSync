5c89510423f4f9d267b00d11014b399783c7b1bd
Franco <cvaldes272@gmail.com>
2024-12-19 19:22:04 -0600

    git commit -m"se agrega"

--- create-file.js
+++ create-file.js
@@ -1,5 +1,4 @@
 //Es un modulo de NODE interno ,
 // no se tiene que instalar ningun paquete y sirve para crear archivos nuevos
 const fs = require("fs");
-
 fs.writeFileSync("Nuevo_archivo.txt", "contenido del archivo", "utf8");
